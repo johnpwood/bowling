66496b5d28d2bd271f8d66685f28a8689a35a013
johnpwood <johnpwoodiv@gmail.com>
2019-12-17 15:59:59 -0600

    add comments and simplify code

--- bowling.exs
+++ bowling.exs
@@ -1,83 +1,94 @@
 defmodule Bowling do
-  defstruct score: 0,
-            frame: 0,
-            start_of_frame?: true,
-            this_bonus: 1,
-            next_bonus: 1,
-            last_roll: 0,
-            game_over?: false
-
   def start do
-    %Bowling{}
+    %{
+      score: 0,
+      frame: 1,
+      last_roll: 0,
+      second_roll_of_frame?: false,
+      current_bonus: 1,
+      next_bonus: 1,
+      game_over?: false
+    }
   end
 
-  def roll(_game, pins) when pins < 0 do
-    {:error, "Negative roll is invalid"}
+  def roll(g, pins) do
+    case(e = error_check(g, pins)) do
+      {:error, _} -> e
+      _ -> update_game(g, pins)
+    end
   end
 
-  def roll(_game, pins) when pins > 10 do
-    {:error, "Pin count exceeds pins on the lane"}
-  end
-
-  def roll(%{last_roll: r, start_of_frame?: false}, pins) when r + pins > 10 do
-    {:error, "Pin count exceeds pins on the lane"}
-  end
-
-  def roll(game = %{frame: f}, pins) when f < 10 do
+  defp update_game(g = %{frame: f}, pins) when f < 11 do
     type =
       cond do
-        pins + game.last_roll == 10 and not game.start_of_frame? -> :spare
+        pins + g.last_roll == 10 and g.second_roll_of_frame? -> :spare
         pins == 10 -> :strike
         true -> :nothing_special
       end
 
     %{
-      score: game.score + pins * game.this_bonus,
+      score: g.score + g.current_bonus * pins,
       frame:
         cond do
-          game.start_of_frame? and pins < 10 -> game.frame
-          true -> game.frame + 1
+          pins < 10 and not g.second_roll_of_frame? -> g.frame
+          true -> g.frame + 1
         end,
-      start_of_frame?: pins == 10 or not game.start_of_frame?,
-      this_bonus:
-        game.next_bonus +
+      second_roll_of_frame?: pins < 10 and not g.second_roll_of_frame?,
+      current_bonus:
+        g.next_bonus +
           case type do
             :strike -> 1
             :spare -> 1
-            :nothing_special -> 0
+            _ -> 0
           end,
       next_bonus:
         1 +
           case type do
             :strike -> 1
-            :spare -> 0
-            :nothing_special -> 0
+            _ -> 0
           end,
       last_roll: pins,
-      game_over?: game.frame == 9 and type == :nothing_special
+      game_over?: g.frame == 10 and type == :nothing_special and g.second_roll_of_frame?
     }
   end
 
-  def roll(%{game_over?: true}, _pins) do
-    {:error, "Cannot roll after game is over"}
-  end
-
-  def roll(game, pins) do
+  defp update_game(g, pins) do
     %{
-      score: game.score + pins * (game.this_bonus - 1),
-      this_bonus: game.next_bonus,
+      score: g.score + pins * (g.current_bonus - 1),
+      current_bonus: g.next_bonus,
       next_bonus: 1,
-      start_of_frame?: pins == 10,
+      second_roll_of_frame?: pins < 10,
       last_roll: pins,
-      game_over?: game.next_bonus == 1
+      game_over?: g.next_bonus == 1
     }
+  end
+
+  defp error_check(g, pins) do
+    # IO.inspect(g)
+
+    cond do
+      pins < 0 ->
+        {:error, "Negative roll is invalid"}
+
+      pins > 10 ->
+        {:error, "Pin count exceeds pins on the lane"}
+
+      g.second_roll_of_frame? and g.last_roll + pins > 10 ->
+        {:error, "Pin count exceeds pins on the lane"}
+
+      g.game_over? ->
+        {:error, "Cannot roll after game is over"}
+
+      true ->
+        :no_error
+    end
   end
 
   def score(%{game_over?: false}) do
     {:error, "Score cannot be taken until the end of the game"}
   end
 
-  def score(game) do
-    game.score
+  def score(g) do
+    g.score
   end
 end
